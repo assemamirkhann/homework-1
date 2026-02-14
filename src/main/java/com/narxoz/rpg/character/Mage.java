cc910f0d94f9456d244ecb54fcaee21fb9f5df5a
assemamirkhann <151700427+assemamirkhann@users.noreply.github.com>
2026-02-14 10:42:14 +0500

    Added character Mage and class extends Character abstract class

---
+++ src/main/java/com/narxoz/rpg/character/Mage.java
@@ -0,0 +1,28 @@
+package com.narxoz.rpg.character;
+
+public class Mage extends Character{
+        // TODO: Add fields for equipped weapon and armor
+        // Think: Should Warrior know about its equipment?
+
+        public Mage(String name) {
+            super(name, 150, 30, 80, 20);
+        }
+
+        // Example method structure:
+        public String getName() {
+            return name;
+        }
+
+        public void displayStats() {
+            System.out.println("=== " + name + " (Mage) ===");
+            System.out.println("Health: " + health);
+            System.out.println("Mana: " + mana);
+            System.out.println("Strength: " + strength);
+            System.out.println("Intelligence: " + intelligence);
+        }
+
+        public void useSpecialAbility() {
+            System.out.println(name + " casts FIREBALLS! Strength temporarily increased!");
+        }
+    }
+
